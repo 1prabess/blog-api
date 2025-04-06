882a2da36830ff2e08cfb4a5bc3a068a8c6e917e
Prabesh Shrestha <sthxprabesh@gmail.com>
2025-04-06 09:26:24 +0545

    Integrated new features

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,23 +1,41 @@
 import express from "express";
 import {
-  getPost,
-  getAllPosts,
   deletePost,
   updatePost,
   handleCreatePost,
+  handleUploadThumbnailPhoto,
+  handleGetAllPosts,
+  handleGetPost,
 } from "../controllers/postsController.js";
 import authenticateTokenMiddleware from "../middlewares/authenticateTokenMiddleware.js";
+import authenticatetokenMiddleware from "../middlewares/authenticateTokenMiddleware.js";
+import storage from "../config/cloudinary.js";
+import multer from "multer";
+const upload = multer({ storage });
 
 const postsRouter = express.Router();
 
+// Create post
 postsRouter.post("/", authenticateTokenMiddleware, handleCreatePost);
 
-postsRouter.get("/:id", getPost);
+// Get your all posts
+postsRouter.get("/", authenticateTokenMiddleware, handleGetAllPosts);
 
-postsRouter.get("/", getAllPosts);
+// Get all post of other user
+postsRouter.get("/user/:userId", handleGetAllPosts);
+
+// Get single post
+postsRouter.get("/post/:postId", handleGetPost);
 
 postsRouter.delete("/:id", deletePost);
 
 postsRouter.put("/:id", updatePost);
+
+postsRouter.post(
+  "/:postId/thumbnail",
+  authenticatetokenMiddleware,
+  upload.single("thumbnail"),
+  handleUploadThumbnailPhoto
+);
 
 export default postsRouter;
