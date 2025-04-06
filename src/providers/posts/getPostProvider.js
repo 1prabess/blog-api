bb4eb958401fcf950e33b991613f184812adef75
Prabesh Shrestha <sthxprabesh@gmail.com>
2025-04-06 14:32:15 +0545

    Fixed minor issues and cleaned up

--- src/providers/posts/getPostProvider.js
+++ src/providers/posts/getPostProvider.js
@@ -5,10 +5,9 @@ const getPostProvider = async (req, res) => {
   const postId = req.params.postId;
   try {
     const posts = await Post.findOne({ _id: postId });
-    console.log(posts);
+
     return res.status(StatusCodes.OK).json(posts);
   } catch (error) {
-    console.error(error);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       reason: "Unable to process the request, please try again later.",
     });
