a21df62ac08ef7823602738f3e1fde4d0103d50f
Prabesh Shrestha <sthxprabesh@gmail.com>
2025-04-06 14:34:09 +0545

    Changed profile photo route and added few comments

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -17,36 +17,48 @@ import storage from "../config/cloudinary.js";
 const usersRouter = express.Router();
 const upload = multer({ storage });
 
+// Register new user
 usersRouter.post("/register", handleRegisterUser);
 
+// Login user
 usersRouter.post("/login", handleLoginUser);
 
+// Get all users
 usersRouter.get("/", handleGetUsers);
 
+// Follow user
 usersRouter.post(
   "/following/:id/",
   authenticatetokenMiddleware,
   handleFollowUser
 );
 
+// Unfollow user
 usersRouter.delete(
   "/following/:id/",
   authenticatetokenMiddleware,
   handleUnfollowUser
 );
 
+// Get your profile
 usersRouter.get("/profile", authenticatetokenMiddleware, handleGetUserProfile);
 
+// Delete your profile
 usersRouter.delete(
   "/profile",
   authenticatetokenMiddleware,
   handleDeleteProfile
 );
 
+// Get profile of other user
+usersRouter.get("/profile/:userId", handleGetUserProfile);
+
+// Update your profile
 usersRouter.put("/profile", authenticatetokenMiddleware, handleUpdateProfile);
 
+// Upload user profile photo
 usersRouter.post(
-  "/profile-photo-upload",
+  "/profilePhoto",
   authenticatetokenMiddleware,
   upload.single("profile"),
   handleUploadProfilePhoto
