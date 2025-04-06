205af80cd6a30096387538785a805eaf53521a97
Prabesh Shrestha <sthxprabesh@gmail.com>
2025-04-06 14:31:45 +0545

    Added category and also added likesCount as virtual property

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -7,34 +7,35 @@ const postSchema = new Schema(
       required: [true, "Post title is required"],
       trim: true,
     },
-
     content: {
       type: String,
       required: [true, "Post content is required"],
     },
-
-    category: {
-      type: Schema.Types.ObjectId,
-      ref: "Category",
-      // required: [true, "Post category is required"],
-    },
-
     likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-
-    dislikes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-
     user: {
       type: Schema.Types.ObjectId,
       ref: "User",
       required: [true, "Post author is required"],
     },
-
     thumbnail: {
       type: String,
     },
+    category: {
+      type: String,
+      required: [true, "Post category is required"],
+      enum: ["technology", "lifestyle", "health", "business"],
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
+
+postSchema.virtual("likesCount").get(function () {
+  return this.likes.length;
+});
 
 const Post = model("Post", postSchema);
 
