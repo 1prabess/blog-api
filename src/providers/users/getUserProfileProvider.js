034e8a49ad47093a24f1c769d0892ba81501a640
Prabesh Shrestha <sthxprabesh@gmail.com>
2025-04-06 14:30:40 +0545

    Fixed

--- src/providers/users/getUserProfileProvider.js
+++ src/providers/users/getUserProfileProvider.js
@@ -2,8 +2,9 @@ import { StatusCodes } from "http-status-codes";
 import User from "../../models/userModel.js";
 
 const getUserProfileProvider = async (req, res) => {
+  const userId = req.params.userId || req.user.sub;
   try {
-    const user = await User.findById(req.user.sub);
+    const user = await User.findById({ _id: userId });
     return res.status(StatusCodes.OK).json(user);
   } catch (error) {
     return res.status(StatusCodes.GATEWAY_TIMEOUT).json({
