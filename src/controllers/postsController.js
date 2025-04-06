b6d4695fe9779db46a8d36b4a13f2ec67ee86450
Prabesh Shrestha <sthxprabesh@gmail.com>
2025-04-06 14:33:30 +0545

    Added handlers for liking and unliking a post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,9 @@
 import createPostProvider from "../providers/posts/createPostProvider.js";
+import getAllPostsOfProfileProvider from "../providers/posts/getAllPostsOfProfileProvider.js";
 import getAllPostsProvider from "../providers/posts/getAllPostsProvider.js";
 import getPostProvider from "../providers/posts/getPostProvider.js";
+import likePostProvider from "../providers/posts/likePostProvider.js";
+import unlikePostProvider from "../providers/posts/unlikePostProvider.js";
 import uploadThumbnailPhotoProvider from "../providers/posts/uploadThumbnailPhotoProvider.js";
 
 export const handleCreatePost = async (req, res) => {
@@ -15,8 +18,20 @@ export const handleGetPost = async (req, res) => {
   await getPostProvider(req, res);
 };
 
+export const handleGetAllPostsOfProfile = async (req, res) => {
+  await getAllPostsOfProfileProvider(req, res);
+};
+
 export const handleGetAllPosts = async (req, res) => {
   await getAllPostsProvider(req, res);
+};
+
+export const handleLikePost = async (req, res) => {
+  await likePostProvider(req, res);
+};
+
+export const handleUnlikePost = async (req, res) => {
+  await unlikePostProvider(req, res);
 };
 
 export const deletePost = async (req, res) => {
